279b9e943d753f057f79548ac59fd8622a0f0ea2
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-29 09:46:10 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kompleks_veri_baglama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList kaynakVeri = new ArrayList();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKitapEkle_Click(object sender, EventArgs e)
+        {
+            Kitaplar kitap = new Kitaplar();
+            kitap.KitapAdi = txtKitapAdi.Text;
+            kitap.KitapYazari = txtKitapYazari.Text;
+            kitap.KitapTuru = txtKitapTuru.Text;
+            kaynakVeri.Add(kitap);
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            listeVeri.DataSource=null;
+            listeVeri.DataSource = kaynakVeri;
+            listeVeri.DisplayMember = "itapAdi";
+        }
+    }
+}
